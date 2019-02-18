60849d2ae203b4d0244c7aca268683778be686b9
Amirul <Amirul@example.com>
2019-02-18 09:22:59 +0800

    this is the edit roe removing the ID

--- resources/views/crud/index.blade.php
+++ resources/views/crud/index.blade.php
@@ -6,7 +6,7 @@
     <table class="table table-striped">
     <thead>
       <tr>
-        <th>ID</th>
+       <!-- <th>ID</th> -->
         <th>Title</th>
         <th>Post</th>
         <th colspan="2">Action</th>
@@ -15,7 +15,7 @@
     <tbody>
       @foreach($cruds as $post)
       <tr>
-        <td>{{$post['id']}}</td>
+       <!--  <td>{{$post['id']}}</td> -->
         <td>{{$post['title']}}</td>
         <td>{{$post['post']}}</td>
         <td><a href="{{action('CRUDController@edit', $post['id'])}}" class="btn btn-warning">Edit</a></td>
