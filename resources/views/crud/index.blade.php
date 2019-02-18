ebf1bd86cd498f7d2919217965393c3de71d38a4
Amirul <Amirul@example.com>
2019-02-18 08:57:59 +0800

    second commit

---
+++ resources/views/crud/index.blade.php
@@ -0,0 +1,23 @@
+@extends('master')
+@section('content')
+  <div class="container">
+    <table class="table table-striped">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Title</th>
+        <th>Post</th>
+      </tr>
+    </thead>
+    <tbody>
+      @foreach($cruds as $post)
+      <tr>
+        <td>{{$post['id']}}</td>
+        <td>{{$post['title']}}</td>
+        <td>{{$post['post']}}</td>
+      </tr>
+      @endforeach
+    </tbody>
+  </table>
+  </div>
+@endsection
